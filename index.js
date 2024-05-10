29f0bcde758eac5a320f1c862d851048af95323d
xdsftgyhuy6 <gs1kaydankk@hotmail.com>
2024-05-10 15:40:59 +0700

    start project

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const reverseString = (str) => str.split("").reverse().join("");
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
+}
