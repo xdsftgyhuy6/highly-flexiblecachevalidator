ca7b3e2e1ed5c530d0c1811e8956e398a427a803
xdsftgyhuy6 <gs1kaydankk@hotmail.com>
2024-05-15 16:51:21 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function rob(nums) {
-  let prevMax = 0;
-  let currMax = 0;
-  for (const num of nums) {
-    const temp = currMax;
-    currMax = Math.max(prevMax + num, currMax);
-    prevMax = temp;
+function detectCycle(head) {
+  let slow = head;
+  let fast = head;
+  let hasCycle = false;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      hasCycle = true;
+      break;
+    }
   }
-  return currMax;
+  if (!hasCycle) return null;
+  slow = head;
+  while (slow !== fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
 }
